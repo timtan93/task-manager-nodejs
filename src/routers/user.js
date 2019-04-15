3c78e65233d0c399f98c0312ee03c634bcb2d2d2
timtan93 <timtan93@gmail.com>
2019-04-15 15:25:31 +0100

    logout

--- src/routers/user.js
+++ src/routers/user.js
@@ -15,13 +15,25 @@ router.post("/users", async (req, res) => {
     }
   });
 
-  router.post('/users/login', async (req, res) => {
+  router.post('/users/login', auth, async (req, res) => {
       try {
         const user = await User.findByCredentials(req.body.email, req.body.password)
         const token = await user.generateAuthToken()
         res.send({user, token})
       }catch (e) {
         res.status(400).send()
+      }
+  })
+
+  router.post('/users/logout', auth, async (req, res)=> {
+      try{
+        req.user.tokens = req.user.tokens.filter((token)=> {
+          return token.token !== req.token
+        })
+        await req.user.save()
+        res.send()
+      } catch (e) {
+        res.status(500).send()
       }
   })
   
