0b8830519923ee426aa872153447996545ab89b4
timtan93 <timtan93@gmail.com>
2019-04-15 12:09:06 +0100

    began using JWT

--- src/index.js
+++ src/index.js
@@ -17,17 +17,10 @@ app.listen(port, () => {
   console.log("Server is up on port " + port);
 });
 
-const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const myFunction = async () => {
-  const password ='Red12345!'
-  const hashedPassword = await bcrypt.hash(password, 8)
-
-  console.log(password)
-  console.log(hashedPassword)
-
-  const isMatch = await bcrypt.compare('Red12345!', hashedPassword)
-  console.log(isMatch)
+  const token = jwt.sign({ _id: 'abc123' }, 'thisismynewcourse')
 }
 
 myFunction()
