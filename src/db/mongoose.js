899a8907c6d59f62159cee5d5f9de7bddca4222f
timtan93 <timtan93@gmail.com>
2019-04-15 09:39:49 +0100

    first commit

---
+++ src/db/mongoose.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-console */
+const mongoose = require("mongoose");
+
+
+mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: true,
+});
+
+
