7d25ccf9c71240547d45318be673bbc2ed0cc453
timtan93 <timtan93@gmail.com>
2019-04-15 14:34:47 +0100

    added middleware folder with auth method for user get request

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const User = require('../models/user')
+const auth = require('../middleware/auth')
 const router = new express.Router()
 
 router.post("/users", async (req, res) => {
@@ -7,7 +8,8 @@ router.post("/users", async (req, res) => {
   
     try {
       await user.save();
-      res.status(201).send(user);
+      const token = await user.generateAuthToken()
+      res.status(201).send({user, token});
     } catch (e) {
       res.status(400).send(e);
     }
@@ -16,19 +18,15 @@ router.post("/users", async (req, res) => {
   router.post('/users/login', async (req, res) => {
       try {
         const user = await User.findByCredentials(req.body.email, req.body.password)
-        res.send(user)
+        const token = await user.generateAuthToken()
+        res.send({user, token})
       }catch (e) {
         res.status(400).send()
       }
   })
   
-  router.get("/users", async (req, res) => {
-    try {
-      const users = await User.find({});
-      res.send(users);
-    } catch (e) {
-      res.status(500).send();
-    }
+  router.get("/users/me", auth, async (req, res) => {
+    res.send(req.user)
   });
   
   router.get("/users/:id", async (req, res) => {
