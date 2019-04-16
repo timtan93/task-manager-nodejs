ca4d95b260236069adc6e096f40a8bebecad21b6
timtan93 <timtan93@gmail.com>
2019-04-16 11:26:24 +0100

    removed avatar from profile get req

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 ///Users/timothytan/mongodb/bin/mongod --dbpath=/Users/timothytan/mongodb-data
-
 const express = require("express");
 require("./db/mongoose");
 
@@ -9,30 +8,6 @@ const taskRouter = require("./routers/task");
 const app = express();
 const port = process.env.PORT || 3000;
 
-const multer = require('multer')
-
-const upload = multer({
-  dest: 'images',
-  limits: {
-    fileSize: 1000000
-  }, 
-  fileFilter(req, file, cb) {
-    if (!file.originalname.match(/\.(doc|docx)$/)){
-      return cb(new Error('Please upload a Word document'))
-    }
-    // cb(new Error ('File must be a PDF'))
-    cb(undefined, true)
-    // cb(undefined)
-  }
-})
-
-
-app.post('/upload', upload.single('upload'), (req, res) => {
-  res.send()
-}, (error, req, res, next) => {
-  res.status(400).send({error: error.message})
-})
-
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
@@ -40,5 +15,3 @@ app.use(taskRouter);
 app.listen(port, () => {
   console.log("Server is up on port " + port);
 });
-
-
