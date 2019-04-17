ac1bd6461ac4d23dc19dba7b8d3d256ffbd5ba22
timtan93 <timtan93@gmail.com>
2019-04-17 09:31:52 +0100

    added JEST npm and wrote 4 basic tests

---
+++ tests/math.test.js
@@ -0,0 +1,22 @@
+const { calculateTip, fahrenheitToCelsius, celsiusToFahrenheit } = require('../src/math')
+
+test('Should calculate total with tip', () => {
+    const total = calculateTip(10, .3)
+    expect(total).toBe(13)
+
+})
+
+test('Should calculate total with default tip', () => {
+    const total = calculateTip(10)
+    expect(total).toBe(12.5)
+})
+
+test('Should convert 32F to 0C', () => {
+    const celsius = fahrenheitToCelsius(32)
+    expect(celsius).toBe(0) 
+})
+
+test ('Should convert 0C to 32F', () => {
+    const fahrenheit = celsiusToFahrenheit(0)
+    expect(fahrenheit).toBe(32)
+})
