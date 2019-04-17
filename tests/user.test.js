97b3d1d0b7f82a3f8170c82fb4c0bf92a25fce7f
timtan93 <timtan93@gmail.com>
2019-04-17 14:15:11 +0100

    use toEqual for supertest as toBe compares object memory

--- tests/user.test.js
+++ tests/user.test.js
@@ -99,3 +99,13 @@ test('Should not delete account for unauthenticated user', async () => {
     .send()
     .expect(401)
 })
+
+test('Should upload avatar image', async () => {
+    await request(app)
+    .post('/users/me/avatar')
+    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .attach('avatar', 'tests/fixtures/profile-pic.jpg')
+    .expect(200)
+    const user = await User.findById(userOneId)
+    expect({}).toEqual({})
+})
